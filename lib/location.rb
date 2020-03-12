1cc60b8b2a70503827fdad0288e063ea1971a4d1
Roger Robertson Jr <rjrjr@pm.me>
2020-03-12 08:51:51 -0700

    Done

--- lib/location.rb
+++ lib/location.rb
@@ -1,3 +1,23 @@
-
 class Location
+
+    attr_reader :location
+    attr_accessor :restaurant, :rent, :address
+    @@all = []
+
+    def initialize(address)
+        @address = address
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_addresses
+        result = []
+        @@all.each do |location|
+            result << location.address
+        end
+        result
+    end
 end
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,20 @@
 
 class Restaurant
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def create_location(address)
+        new_location = Location.new(address)
+        new_location.restaurant = self
+        new_location
+    end
 end
